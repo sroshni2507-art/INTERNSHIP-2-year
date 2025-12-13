dbbbfda7fe743063fa91ba3a5a9d803453fb2909
S.Roshni <sroshni2507@gmail.com>
2025-12-13 22:28:20 +0530

    Update input fields and prediction logic in app.py

--- Logistic_Regression_Diabetes_Prediction/Logistic_Regression_Diabetes_Prediction/streamlit_app/app.py
+++ Logistic_Regression_Diabetes_Prediction/Logistic_Regression_Diabetes_Prediction/streamlit_app/app.py
@@ -1,25 +1,27 @@
 import streamlit as st
 import pickle
 import numpy as np
-
 import os
-import pickle
 
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "diabetes_model.pkl")
 model = pickle.load(open(MODEL_PATH, "rb"))
 
 st.title("Diabetes Prediction using Logistic Regression")
 
-age = st.number_input("Age", min_value=1, max_value=120)
-mass = st.number_input("Body Mass Index (BMI)", min_value=0.0)
-insu = st.number_input("Insulin Level", min_value=0.0)
-plas = st.number_input("Plasma Glucose", min_value=0.0)
+preg = st.number_input("Pregnancies", 0)
+glucose = st.number_input("Glucose Level", 0)
+bp = st.number_input("Blood Pressure", 0)
+skin = st.number_input("Skin Thickness", 0)
+insulin = st.number_input("Insulin", 0)
+bmi = st.number_input("BMI", 0.0)
+dpf = st.number_input("Diabetes Pedigree Function", 0.0)
+age = st.number_input("Age", 0)
 
 if st.button("Predict"):
-    input_data = np.array([[age, mass, insu, plas]])
-    prediction = model.predict(input_data)
+    input_data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
+    result = model.predict(input_data)
 
-    if prediction[0] == "tested_positive":
+    if result[0] == 1:
         st.error("Person is Diabetic")
     else:
         st.success("Person is Not Diabetic")
