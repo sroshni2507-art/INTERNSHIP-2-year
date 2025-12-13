9bed9de92c1ce890c00ff460541e7b439a2b2a9e
S.Roshni <sroshni2507@gmail.com>
2025-12-13 21:29:14 +0530

    Add Streamlit app for diabetes prediction

---
+++ Logistic_Regression_Diabetes_Prediction/Logistic_Regression_Diabetes_Prediction/streamlit_app/app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Load trained model
+model = pickle.load(open("diabetes_model.pkl", "rb"))
+
+st.title("Diabetes Prediction using Logistic Regression")
+
+age = st.number_input("Age", min_value=1, max_value=120)
+mass = st.number_input("Body Mass Index (BMI)", min_value=0.0)
+insu = st.number_input("Insulin Level", min_value=0.0)
+plas = st.number_input("Plasma Glucose", min_value=0.0)
+
+if st.button("Predict"):
+    input_data = np.array([[age, mass, insu, plas]])
+    prediction = model.predict(input_data)
+
+    if prediction[0] == "tested_positive":
+        st.error("Person is Diabetic")
+    else:
+        st.success("Person is Not Diabetic")
