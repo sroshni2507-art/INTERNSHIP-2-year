5de653c9c77a6e30594a160e1bc779d9850647e7
S.Roshni <sroshni2507@gmail.com>
2025-12-19 23:25:12 +0530

    Update app.py

--- navine_bayes/app.py
+++ navine_bayes/app.py
@@ -5,7 +5,7 @@ import os
 
 # -------------------- LOAD MODEL SAFELY --------------------
 BASE_DIR = os.path.dirname(__file__)
-MODEL_PATH = os.path.join(BASE_DIR, "naive_bayes.pkl")
+MODEL_PATH = os.path.join(BASE_DIR, "naive_bayes_model.pkl")
 
 with open(MODEL_PATH, "rb") as f:
     model = pickle.load(f)
